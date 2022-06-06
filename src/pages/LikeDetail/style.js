e954234f6de2a376a45fd09187498b30e048a196
gpgun0 <rjsgh7943@gmail.com>
2022-06-06 21:59:26 +0900

    design: 코멘트 디자인

--- src/pages/LikeDetail/style.js
+++ src/pages/LikeDetail/style.js
@@ -21,6 +21,58 @@ const Container = styled.div`
       background-size: cover;
     }
   }
+
+  .comment-list {
+    overflow: scroll;
+    width: 100vw;
+    height: 50vh;
+    .comment {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+
+      .comment-text {
+        margin-left: 10px;
+        background-color: lightgray;
+        padding: 15px;
+        border-radius: 20px;
+      }
+    }
+  }
+
+  .comment-form {
+    padding: 5px;
+    position: absolute;
+    bottom: 50px;
+    width: 100%;
+    border-top: 1px solid lightgray;
+
+    .wrapper-in-form {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .comment-input {
+        padding: 10px;
+        max-height: 34px;
+        box-sizing: border-box;
+        height: auto;
+        width: 70vw;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        resize: none;
+      }
+      .comment-input-btn {
+        border: none;
+        border-radius: 5px;
+        height: 34px;
+        width: 60px;
+        margin-left: 10px;
+        background-color: #4eabd2;
+        font-weight: bolder;
+        color: aliceblue;
+      }
+    }
+  }
 `;
 
 export { Container };
